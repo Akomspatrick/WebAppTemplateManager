007dba50148ab76290a814d1b1ebebca39985b63
SW Eng <Sweng@ad.massload.com>
2023-09-13 11:29:49 -0600

    initial commit

---
+++ src/DocumentVersionManager.Domain/ModelAggregateRoot/Entities/Model.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentVersionManager.Domain.ModelAggregateRoot.Entities
+{
+    public class Model
+    {
+        //name`,`modelTypes_name`
+        public string ModelId { get; init; } = string.Empty;
+        public string ModelName { get; init; } = string.Empty;
+    }
+
+}
---
+++ src/DocumentVersionManager.Domain/ModelAggregateRoot/ModelAggregateRoot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentVersionManager.Domain.ModelAggregateRoot
+{
+    public partial class Model
+    {
+        public Model(string modelId, string modelName)
+        {
+            modelId = modelId;
+            modelName = modelName;
+        }
+    }
+}
