9bf819ccdc6c5015ba56fc2c55acea3b3b893995
SW ENG <sweng@massload.com>
2023-10-13 12:30:17 -0600

    Getting pdf or image with barcode

--- WebApplication1/Controllers/WeatherForecastController.cs
+++ WebApplication1/Controllers/WeatherForecastController.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Reporting.NETCore;
 using System.Drawing;
 using System.Drawing.Imaging;
 using ZXing;
 using ZXing.Common;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
 
 namespace WebApplication1.Controllers
 {
@@ -16,37 +18,108 @@ namespace WebApplication1.Controllers
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        private IHostingEnvironment _hostingEnvironment;
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IHostingEnvironment hostingEnvironment)
         {
             _logger = logger;
+            _hostingEnvironment = hostingEnvironment;   
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        //[HttpGet(Name = "GetWeatherForecast")]
+        //public IEnumerable<WeatherForecast> Get()
+        //{
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+        //        TemperatureC = Random.Shared.Next(-20, 55),
+        //        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        ////}
+        //[HttpPost(Name = "GetWeatherForecastqqq")]
+        //public IActionResult GenerateBarcode(string text)
+        //{
+
+        //    //var writer = new ZXing.Windows.Compatibility.BarcodeWriter() { Format = BarcodeFormat.QR_CODE };
+
+
+        //    ZXing.Windows.Compatibility.BarcodeWriter barcodeWriter = new ZXing.Windows.Compatibility.BarcodeWriter
+        //    {
+        //        Format = BarcodeFormat.CODE_128, // Specify the barcode format you want to generate
+        //        Options = new EncodingOptions
+        //        {
+        //            Height = 200, // Specify the height of the barcode image
+        //            Width = 400 ,
+        //            // Specify the width of the barcode image,
+        //        }
+        //    };
+
+        //    Bitmap barcodeBitmap = barcodeWriter.Write(text); // Generate the barcode image
+
+        //    MemoryStream stream = new MemoryStream();
+        //    barcodeBitmap.Save(stream, ImageFormat.Png); // Save the barcode image to a stream
+
+        //    return File(stream.ToArray(), "image/png"); // Return the barcode image as a file
+        //}
+
+
+
+        //}
+        
+        [HttpGet(Name = "GetWeatherForecastqqq")]
+       // public IActionResult PrintLabel([FromBody] TheProduct product)
+       public IActionResult PrintLabel(string  productId)
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+
+            string string64 = GetBarCodeIStream(productId);
+            var path = Path.Combine(_hostingEnvironment.ContentRootPath, "Reports", "Report1.rdlc");
+
+            using (var stream = new FileStream(path, FileMode.Open, FileAccess.Read))
             {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+                LocalReport locareport = new();
+                locareport.LoadReportDefinition(stream);
+                locareport.EnableExternalImages = true;
+
+                ReportParameterCollection reportParameters = new();
+                reportParameters.Add(new ReportParameter("ProductId", productId));
+                reportParameters.Add(new ReportParameter("BarCodeImg", string64,true));
+                locareport.SetParameters(reportParameters);
+                //var result = locareport.Render("PDF");
+                var result = locareport.Render("IMAGE");
+                return File(result, "image/png", "PrintOut.png");
+
+                //* return File(result, "application/pdf", "PrintOut.pdf");
+                //•	"IMAGE"
+                //•	"WORD"/
+                //•	"EXCEL"
+                //•	"CSV"
+                //•	"XML"
+                //•	"HTML4.0"
+                //•	"HTML3.2"
+                //•	"MHTML" *//* }
+               // return File(result, "application/pdf", "PrintOut.pdf");
+            }
+
+
         }
-        [HttpPost(Name = "GetWeatherForecastqqq")]
-        public IActionResult GenerateBarcode(string text)
+
+
+
+
+
+
+
+
+        private static string GetBarCodeIStream(string text)
         {
-
-            //var writer = new ZXing.Windows.Compatibility.BarcodeWriter() { Format = BarcodeFormat.QR_CODE };
-
-
             ZXing.Windows.Compatibility.BarcodeWriter barcodeWriter = new ZXing.Windows.Compatibility.BarcodeWriter
             {
                 Format = BarcodeFormat.CODE_128, // Specify the barcode format you want to generate
                 Options = new EncodingOptions
                 {
                     Height = 200, // Specify the height of the barcode image
-                    Width = 400 // Specify the width of the barcode image
+                    Width = 400,
+                    // Specify the width of the barcode image,
                 }
             };
 
@@ -55,7 +128,11 @@ namespace WebApplication1.Controllers
             MemoryStream stream = new MemoryStream();
             barcodeBitmap.Save(stream, ImageFormat.Png); // Save the barcode image to a stream
 
-            return File(stream.ToArray(), "image/png"); // Return the barcode image as a file
+            // return File(stream.ToArray(), "image/png"); // Return the barcode image as a file
+            var byteArray = stream.ToArray();
+            // return byteArray;
+            return Convert.ToBase64String(byteArray);
+          
         }
     }
 }
