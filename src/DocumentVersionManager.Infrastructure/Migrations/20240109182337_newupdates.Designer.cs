7f3f7eca734955147093a035956d0f0428546936
SW ENG <sweng@massload.com>
2024-01-09 17:28:22 -0600

    migrated by mistake

--- src/DocumentVersionManager.Infrastructure/Migrations/20240109182337_newupdates.Designer.cs
+++ src/DocumentVersionManager.Infrastructure/Migrations/20240109182337_newupdates.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using DocumentVersionManager.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DocumentVersionManager.Infrastructure.Migrations
 {
     [DbContext(typeof(DocumentVersionManagerContext))]
-    partial class DocumentVersionManagerContextModelSnapshot : ModelSnapshot
+    [Migration("20240109182337_newupdates")]
+    partial class newupdates
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
